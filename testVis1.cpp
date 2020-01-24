eb4ab8c5269468573b83c332a304283e6b369655
Kuba7331 <kuba@kamionka.eu>
2020-01-24 22:50:21 +0100

    First Visual Studio script!

---
+++ testVis1.cpp
@@ -0,0 +1,24 @@
+﻿
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void incrementValue() {
+	int x, wybor;
+	cout << "Prosze podac wartosc" << endl;
+	cin >> x;
+	cout << "Wartosc przed inkrementacja: " << x << endl;
+	cout << "Wartosc po inkrementacji: " << ++x << endl;
+	getchar(); getchar();
+	system("cls");
+
+}
+
+int main()
+{
+	incrementValue();
+	return 0;
+}
+
